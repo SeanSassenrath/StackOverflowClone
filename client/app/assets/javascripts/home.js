0f0e30746f4f3c56770372d58210b7407a0e4463
SeanSassenrath <sean.sassenrath@gmail.com>
2015-05-24 12:51:36 -0700

    adding handlebars question list template

--- client/app/assets/javascripts/home.js
+++ client/app/assets/javascripts/home.js
@@ -1,9 +1,11 @@
 $(document).on('page:change', function(){
+  var source = $('#question_list_template').html();
+  var template = Handlebars.compile(source);
   $.ajax({
     url: 'http://localhost:4000',
     type: 'get'
   }).done(function(response){
-    console.log(response);
+    $('#question_list').append(template({question: response}));
   }).fail(function(response){
     console.log("WHYYYYYYY");
   });
