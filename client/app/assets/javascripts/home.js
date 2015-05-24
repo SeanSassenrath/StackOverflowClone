7395e3597a006fda19aa494f72be3ef64602ba29
SeanSassenrath <sean.sassenrath@gmail.com>
2015-05-24 13:38:38 -0700

    re-commit

--- client/app/assets/javascripts/home.js
+++ client/app/assets/javascripts/home.js
@@ -2,7 +2,7 @@ $(document).on('page:change', function(){
   var source = $('#question_list_template').html();
   var template = Handlebars.compile(source);
   $.ajax({
-    url: 'http://localhost:4000',
+    url: 'http://localhost:4000/questions',
     type: 'get'
   }).done(function(response){
     $('#question_list').append(template({question: response}));
