3a820aa429ddaf96864cd9f84744489c8aafe499
SeanSassenrath <sean.sassenrath@gmail.com>
2015-05-24 11:47:28 -0700

    finishing backend setup

--- client/config/routes.rb
+++ client/config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-
-  resources :questions
-  resources :answers
-  resources :users
-
-  root "questions#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
